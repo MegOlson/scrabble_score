e617741aa5813498cc61a4c5c7689b753e72e84f
Christine Callo and Megan Olson <christine@>
2017-09-05 15:10:31 -0700

    add all specs and code

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,5 +1,25 @@
 class Score
   def scrabble_score
-    self.split('')
+    array = self.split('')
+    array = array.map do |e|
+      e == 1
+      if e == "d" || e == "g"
+        2
+      # elsif e == "b" || e == "c" || e == "m" || e == "p"
+      #   3
+      # elsif e == "f" || e == "h" || e == "v" || e == "w" || e == "y"
+      #   4
+      # elsif e == "k"
+      #   5
+      # elsif e == "j" || e == "x"
+      #   8
+      # elsif e == "q" || e == "z"
+      #   10
+      else
+        1
+      end
+    end
+   sum = array.inject(:+)
+   sum
   end
 end
--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -2,7 +2,25 @@ require('rspec')
 require('scrabble_score')
 
 describe('#scrabble_score') do
-  it("returns scrabble score for a letter") do
+  it("returns scrabble score of 1 for all letters") do
     expect(("a").scrabble_score()).to(eq(1))
   end
+  it("returns scrabble score of 2 for letters D and G") do
+    expect(("d").scrabble_score()).to(eq(2))
+  end
+#   it("returns scrabble score of 3 for letters B, C, M, P") do
+#     expect(("b").scrabble_score()).to(eq(3))
+#   end
+#   it("returns scrabble score of 4 for letters F, H, V, W, Y") do
+#     expect(("h").scrabble_score()).to(eq(4))
+#   end
+#   it("returns scrabble score of 5 for the letter K") do
+#     expect(("k").scrabble_score()).to(eq(5)
+#   end
+#   it("returns scrabble score of 8 for letters J and X") do
+#     expect(("j").scrabble_score()).to(eq(8))
+#   end
+#   it("returns scrabble score of 10 for letters Q and Z") do
+#     expect(("q").scrabble_score()).to(eq(10))
+#   end
 end
